e87191bc3410e2dd28da1cbfaec23fcbd45e47a5
Corchil Kelly Kwame <85358793+De-Akatsuki@users.noreply.github.com>
2024-04-29 22:01:46 +0000

    Add files via upload

---
+++ Data Science Ecosystem.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# ### In this Notebook, Data Science Tools and Ecosystem are summarized.
+
+# ### OBJECTIVES:
+# - Create a Jupyter Notebook with markdown and code cells
+# - List examples of languages, libraries and tools used in Data Science
+# - Share your Jupyter Notebook publicly on GitHub
+# - Evaluate notebooks submitted by your peers using the provided rubric
+# - Demonstrate proficiency in Data Science toolkit knowledge
+
+# #### Some of the popular languages that DataScientists use are:
+# 
+# 1. Python
+# 2. Java Script
+# 3. R
+
+# #### Some of the commonly used libraries used by Data Scientists include:
+# 
+# 1. Pandas
+# 2. Numpy
+# 3. Matplotlib
+# 4. Scikitlearn
+
+# |Data Science Tools|
+# |:-: |
+# |Google Co-Lab|
+# |Jupyter Notebook|
+# |IBM Watson|
+# 
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python.
+
+# In[4]:
+
+
+## This is a simple arithemitic expression to multiply then add integers.
+
+(3*4)+5
+
+
+# In[10]:
+
+
+## This will convert 200 minutes to hours by diving by 60.
+
+def minutes_to_hours(minutes):
+    
+    hours = minutes / 60
+    return hours
+
+# Convert 200 minutes to hours
+minutes = 200
+hours = minutes_to_hours(minutes)
+print(f"{minutes} minutes is equal to {hours} hours.")
+
+
+
+
+# ## Author
+# Corchil Kelly Kwame
